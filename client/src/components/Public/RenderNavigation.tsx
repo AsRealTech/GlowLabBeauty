f1e0af850ff13b811121e7ed2fd0ae1c780263ef
AsRealTech <asrealzy@gmail.com>
2026-02-25 02:53:20 -0800

    styled drizllecommit

--- client/src/components/Public/RenderNavigation.tsx
+++ client/src/components/Public/RenderNavigation.tsx
@@ -3,13 +3,14 @@ export function RenderNavigation() {
     return (
 
         <nav>
-            <a href="/" className="logo">GlowLab Beauty</a>
-            <ul>
+            <a href="/" className="logo" style={{fontSize:"20px"}}>GlowLab Beauty</a>
+            <ul style={{display:"flex", gap:"20px"}}>
                 <li><a href="/">Home</a></li>
                 <li><a href="#ingredients">Ingredients</a></li>
                 <li><a href="#products">Products</a></li>
                 <li><a href="#tips">Tips</a></li>
                 <li><a href="#">About</a></li> 
+                <li><a href="/login">Login</a></li> 
             </ul>   
         </nav>
     
