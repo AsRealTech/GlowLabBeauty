a04034e0cfadc12ea36058b050522ffb126f8878
AsRealTech <114587693+AsRealTech@users.noreply.github.com>
2026-02-27 01:38:55 -0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 // project imports
 import router from 'routes';
 
-// -----------------------|| APP ||-----------------------//
+// -----------------------|| ||-----------------------//
 
 export default function App() {
   return <RouterProvider router={router} />;
